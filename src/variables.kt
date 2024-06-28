a9aba5e3de2227dc7357cbfbdb46402bd794409a
GabrielHrz <gabrielalexanderhr@gmail.com>
2024-06-28 13:58:51 +0100

    Variables.

--- src/variables.kt
+++ src/variables.kt
@@ -1,5 +1,20 @@
+
+//Creando una constante global que funciona en tiempo de compilacion
+const val curso = "Curso de kotlin"
+
+class Usuario{
+
+}
 fun main(){
     //val - variables inmutables.
     //var - variables no mutables.
-    println("Hello World");
+    println("Hello World")
+
+    val nombre = "Jamilton"
+    val apellido = "Silva"
+
+    val edad : Int = 22
+
+
+    println("${nombre} ${apellido}")
 }
