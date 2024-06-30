c748afa894f0da1ac3405bf7550fe1ac7578a212
GabrielHrz <gabrielalexanderhr@gmail.com>
2024-06-30 03:12:26 +0100

    Null Safety

---
+++ src/nullsagety.kt
@@ -0,0 +1,23 @@
+class Carro{
+
+      var color = "Vermelho"
+      fun acelerar(){
+
+      }
+
+}
+
+fun main(){
+
+    val carro : Carro? = null
+    //val color = carro?.color ?: "color padron"
+    if (carro != null) {
+        println(carro.color)
+    }
+
+}
+
+fun clique(){
+
+
+}
