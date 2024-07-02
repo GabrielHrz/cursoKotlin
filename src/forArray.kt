5a1098ea4eeafe0a499a47afadf96e1b9d34c664
GabrielHrz <gabrielalexanderhr@gmail.com>
2024-07-02 01:39:07 +0100

    Arrays e ciclos for.

---
+++ src/forArray.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    val postagem = arrayOf("Viagem para a praia","Levei meu cachorro no veterinario", "Trilha com amigos")
+
+    for ((indice,mensaje) in postagem.withIndex()){
+        println("Indice $indice con el mensaje : $mensaje")
+
+
+    }
+
+
+
+
+}
